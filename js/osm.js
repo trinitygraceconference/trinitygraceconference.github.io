57f5306cf16c47db1a58b38cc5d81bc7def04a17
Trinity Grace Conference <trinitygraceconference@gmail.com>
2019-12-09 23:23:41 +0000

    Rebuilding Mon Dec  9 23:23:41 GMT 2019

---
+++ js/osm.js
@@ -0,0 +1,38 @@
+var map;
+window.addEventListener("DOMContentLoaded", function () {
+    // The overlay layer for our marker, with a simple diamond as symbol
+    var overlay = new OpenLayers.Layer.Vector('Overlay', {
+        styleMap: new OpenLayers.StyleMap({
+            externalGraphic: 'http://dev.openlayers.org/img/marker.png',
+            graphicWidth: 20, graphicHeight: 24, graphicYOffset: -24,
+            title: '${tooltip}'
+        })
+    });
+
+    // The location of our marker and popup. We usually think in geographic
+    // coordinates ('EPSG:4326'), but the map is projected ('EPSG:3857'). //53.6480134,-2.3169703
+    var myLocation = new OpenLayers.Geometry.Point(-2.3148, 53.6480134)
+        .transform('EPSG:4326', 'EPSG:3857');
+
+    // We add the marker with a tooltip text to the overlay
+    overlay.addFeatures([
+        new OpenLayers.Feature.Vector(myLocation, {tooltip: 'Trinity Grace Church'})
+    ]);
+/*
+    // A popup with some information about our location
+    var popup = new OpenLayers.Popup.FramedCloud("Popup", 
+        myLocation.getBounds().getCenterLonLat(), null,
+        '<a target="_blank" href="http://openlayers.org/">We</a> ' +
+        'could be here.<br>Or elsewhere.', null,
+        true // <-- true if we want a close (X) button, false otherwise
+    );*/
+
+    // Finally we create the map
+    map = new OpenLayers.Map({
+        div: "map", projection: "EPSG:3857",
+        layers: [new OpenLayers.Layer.OSM(), overlay],
+        center: myLocation.getBounds().getCenterLonLat(), zoom: 17
+    });
+    // and add the popup to it.
+    //map.addPopup(popup);
+});
